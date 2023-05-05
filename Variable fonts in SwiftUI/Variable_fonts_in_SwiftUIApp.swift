5d2ecc7e27ab144fb0c6028d7f5bcadaf79e30ee
Oleg Frolov <frolololeg@gmail.com>
2023-05-05 20:13:57 +0100

    Initial Commit

---
+++ Variable fonts in SwiftUI/Variable_fonts_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Variable_fonts_in_SwiftUIApp.swift
+//  Variable fonts in SwiftUI
+//
+//  Created by Oleg Frolov on 05/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Variable_fonts_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
